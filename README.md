13e63f0cdfa3ae5852fdb0bbb1ecb50a5f98648c
Breno Castelo Branco <breno.cbsousa@gmail.com>
2022-12-07 14:12:53 +0000

    Bootstraping design system strucutre

---
+++ README.md
@@ -0,0 +1,11 @@
+# Components
+
+- [ ] Text
+- [ ] Heading
+- [ ] Box
+- [ ] Button
+- [ ] TextInpu
+- [ ] TextArea
+- [ ] CheckBox
+- [ ] Avatar
+- [ ] MultiStep
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,9 @@
+export * from './colors';
+
+export * from './fonts';
+export * from './font-sizes';
+export * from './font-weight';
+
+export * from './line-heights';
+export * from './radii';
+export * from './space';
